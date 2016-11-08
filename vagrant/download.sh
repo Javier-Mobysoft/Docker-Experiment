644101b07b7a274e6bd6224aec33066d6c0e7321
Javier <javier.gomez@mobysoft.com>
2016-11-08 11:01:23 +0000

    Added Account Management

--- vagrant/download.sh
+++ vagrant/download.sh
@@ -2,11 +2,14 @@
 
 set -e
 
-echo "Downloading Mobysoft artifact. This will take a while..."
+echo "Downloading Mobysoft artifacts. This will take a while..."
 
 rm -f /vagrant/AuthenticationService/AuthenticationService.war &> /dev/null
 rm -f /vagrant/AuthenticationService/SecurityAdminService.war &> /dev/null
+rm -f /vagrant/AuthenticationService/AccountManagement.war &> /dev/null
 
 mvn org.apache.maven.plugins:maven-dependency-plugin:2.8:get -Dartifact=com.mobysoft.security:AuthenticationService:1.0-SNAPSHOT:war -DremoteRepositories=http://nexus.mobyops.uk/nexus/content/groups/public/ -Ddest=/vagrant/AuthenticationService/AuthenticationService.war &> /dev/null
 
 mvn org.apache.maven.plugins:maven-dependency-plugin:2.8:get -Dartifact=com.mobysoft.security:SecurityAdminService:1.0-SNAPSHOT:war -DremoteRepositories=http://nexus.mobyops.uk/nexus/content/groups/public/ -Ddest=/vagrant/SecurityAdminService/SecurityAdminService.war &> /dev/null
+
+mvn org.apache.maven.plugins:maven-dependency-plugin:2.8:get -Dartifact=com.mobysoft.security:AccountManagement:1.0-SNAPSHOT:war -DremoteRepositories=http://nexus.mobyops.uk/nexus/content/groups/public/ -Ddest=/vagrant/AccountManagement/AccountManagement.war &> /dev/null
