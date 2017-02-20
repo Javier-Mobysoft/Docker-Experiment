39962a456424a0d0069f66fc3750e7f2f050aef5
Javier <javier.gomez@mobysoft.com>
2017-02-20 09:59:45 +0000

    Added IRR, legacy

--- vagrant/download.sh
+++ vagrant/download.sh
@@ -8,6 +8,8 @@ rm -f /vagrant/Notifications/repository/Repository.war &> /dev/null
 rm -f /vagrant/Notifications/exchange/Exchange.war &> /dev/null
 rm -f /vagrant/Notifications/loader/Loader.war &> /dev/null
 rm -f /vagrant/Notifications/surveys/Surveys.war &> /dev/null
+rm -f /vagrant/IRR/surveys/IRR.war &> /dev/null
+rm -f /vagrant/Legacy/bi/BI.war &> /dev/null
 
 mvn org.apache.maven.plugins:maven-dependency-plugin:2.8:get -Dartifact=com.mobysoft.notifications:Repository:LATEST:war -DremoteRepositories=http://nexus.mobyops.uk/nexus/content/groups/public/ -Ddest=/vagrant/Notifications/repository/Repository.war &> /dev/null
 
@@ -16,3 +18,9 @@ mvn org.apache.maven.plugins:maven-dependency-plugin:2.8:get -Dartifact=com.moby
 mvn org.apache.maven.plugins:maven-dependency-plugin:2.8:get -Dartifact=com.mobysoft.notifications:Loader:LATEST:war -DremoteRepositories=http://nexus.mobyops.uk/nexus/content/groups/public/ -Ddest=/vagrant/Notifications/loader/Loader.war &> /dev/null
 
 mvn org.apache.maven.plugins:maven-dependency-plugin:2.8:get -Dartifact=com.mobysoft.notifications:Surveys:LATEST:war -DremoteRepositories=http://nexus.mobyops.uk/nexus/content/groups/public/ -Ddest=/vagrant/Notifications/surveys/Surveys.war &> /dev/null
+
+mvn org.apache.maven.plugins:maven-dependency-plugin:2.8:get -Dartifact=com.mobysoft.mobilelogic:RentAnalysisWorker:LATEST:war -DremoteRepositories=http://nexus.mobyops.uk/nexus/content/groups/public/ -Ddest=/vagrant/Legacy/rentsense/RentAnalysisWorker.war
+
+mvn org.apache.maven.plugins:maven-dependency-plugin:2.8:get -Dartifact=com.mobysoft.internal.irr.web:Console:LATEST:war -DremoteRepositories=http://nexus.mobyops.uk/nexus/content/groups/public/ -Ddest=/vagrant/IRR/IRR.war &> /dev/null
+
+mvn org.apache.maven.plugins:maven-dependency-plugin:2.8:get -Dartifact=com.mobysoft.mobilelogic:BI:LATEST:war -DremoteRepositories=http://nexus.mobyops.uk/nexus/content/groups/public/ -Ddest=/vagrant/Legacy/bi/BI.war &> /dev/null
